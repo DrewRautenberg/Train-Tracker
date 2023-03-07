aebae32c711441a542c48df3edbc539f99cd7f31
Drew Rautenberg <drew@drewrautenberg.com>
2023-03-06 23:35:37 -0600

    got an output

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import requests
+import json
+import key
+
+map_id = 40260
+api_key = key.key
+api_url = f"https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={api_key}&mapid={map_id}&max=2&outputType=JSON"
+response = requests.get(api_url)
+data = json.loads(response.content)
+train = data['ctatt']['eta'][0]
+arrival_time = train['arrT']
+print(f"Next train arriving at {arrival_time}")
